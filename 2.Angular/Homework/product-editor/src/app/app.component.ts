08550b3df9d13a55fa7e4b8c43a5d2fc9e50fb39
eshankuthu <kuthu.eshan@gmail.com>
2017-10-12 15:01:51 -0500

    Adding product and viewing in template

--- 2.Angular/Homework/product-editor/src/app/app.component.ts
+++ 2.Angular/Homework/product-editor/src/app/app.component.ts
@@ -1,43 +1,37 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-root',
-//   template: `
-//     <p>
-//       app Works!
-//     </p>
-//   `,
-//   styles: []
-// })
-// export class AppComponent {
-//   title = 'app';
-// }
-
-//**************************** */
-
 import { Component } from '@angular/core';
-
-export class Hero {
- id: number;
- name: string;
+type conditiontype ="new" | "used" | "discontinued";
+export class Product{
+  id:number;
+  name:string;
+  description:string;
+  price:number;
+  condition: conditiontype
+  category:string;
 }
 
 @Component({
- selector: 'my-app',
- template: `
-   <h1>{{title}}</h1>
-   <h2>{{hero.name}} details!</h2>
-   <div><label>id: </label>{{hero.id}}</div>
-   <div>
-     <label>name: </label>
-     <input [(ngModel)]="hero.name" placeholder="name">
-   </div>
-   `
+  selector: 'app-root',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{product.name}} Details</h2>
+    <div><label>id :</label>{{product.id}}</div>
+    <div><label>name :</label>{{product.id}}</div>
+    <div><label>description :</label>{{product.description}}</div>
+    <div><label>price :</label>{{product.price}}</div>
+    <div><label>condition :</label>{{product.condition}}</div>
+    <div><label>category :</label>{{product.category}}</div>
+  `,
+  styles: []
 })
 export class AppComponent {
- title = 'Tour of Heroes';
- hero: Hero = {
-   id: 1,
-   name: 'Windstorm'
- };
+  title = 'Tour of Product';
+  
+   product: Product={
+     id:1,
+     name:'iphone',
+     description:'Iphone 8 New Model 8 month old',
+     price:89999,
+     condition:"new",
+     category:'mobile'
+   }
 }
--- 2.Angular/Homework/product-editor/src/app/app.module.ts
+++ 2.Angular/Homework/product-editor/src/app/app.module.ts
@@ -1,35 +1,20 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-// import { AppComponent } from './app.component';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent
-//   ],
-//   imports: [
-//     BrowserModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-//************************** */
-
-import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
- 
-import { AppComponent }  from './app.component';
- 
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
-  ],
   declarations: [
-    AppComponent
+    AppComponent, FormsModule
   ],
-  bootstrap: [ AppComponent ]
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
